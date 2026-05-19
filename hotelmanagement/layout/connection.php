c8935790c992da050cbaf6bbef67712bb077afbc
T. Bunleap <skylarerror404@gmail.com>
2026-05-19 11:57:44 +0800

    Update connection.php

--- hotelmanagement/layout/connection.php
+++ hotelmanagement/layout/connection.php
@@ -1,8 +1,8 @@
 <?php
 $port = 3306;
-$dbname = 'phphotel';
-$user = 'phpboss';
-$pwd = 'bbu7';
+$dbname = '[YOUR_DATABASE_NAME]';
+$user = '';
+$pwd = '';
 
 $pdo = new PDO("mysql:host=localhost;port=$port;dbname=$dbname", $user, $pwd);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
